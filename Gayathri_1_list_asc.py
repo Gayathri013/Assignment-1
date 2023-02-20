9f39926a34a2f149cf106995dfc24e6c712c1cd1
Gayathri Kallam <68979554+Gayathri013@users.noreply.github.com>
2023-02-20 14:30:05 +0530

    Delete Gayathri_1_list_asc.py

--- Gayathri_1_list_asc.py
+++
@@ -1,7 +0,0 @@
-# taking names as input and splitting the words and converting it to list
-names_li = list(map(str,input().split()))
-# inbuild function to sort the list in ascending order
-names_li.sort()
-# prints list of names in ascending order 
-print(names_li)
-# ["abc" , "bcd" , "cde" , "def"]
